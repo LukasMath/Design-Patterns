8385a2ea04929e73fafef5d00b5e1ecd4106f538
lukas <lukas_matheus@outlook.com>
2019-02-10 22:12:24 -0300

    Design-Patterns

---
+++ Design Patters/src/Finalizado.java
@@ -0,0 +1,25 @@
+
+public class Finalizado implements EstadoDeUmOrcamento {
+
+	@Override
+	public void aplicaDescontoExtra(Orcamento orcamento) {
+		throw new RuntimeException("Orcamentos finalizados não recebem desconto extra!");
+	}
+
+	@Override
+	public void aprova(Orcamento orcamento) {
+		throw new RuntimeException("Orçamento já finalizado!");
+	}
+
+	@Override
+	public void reprova(Orcamento orcamento) {
+		throw new RuntimeException("Orçamento já finalizado!");		
+	}
+
+	@Override
+	public void finaliza(Orcamento orcamento) {
+		throw new RuntimeException("Orçamento já finalizado!");		
+	}
+	
+	
+}
---
+++ Design Patters/src/TemplateDeImpostoCondicional.java
@@ -0,0 +1,17 @@
+public abstract class TemplateDeImpostoCondicional extends Imposto {
+
+	public final double calcula(Orcamento orcamento) {
+
+		if (deveUsarMaximaTaxacao(orcamento)) {
+			return maximaTaxacao(orcamento) + calculoDoOutroImposto(orcamento);
+		} else {
+			return minimaTaxacao(orcamento) + calculoDoOutroImposto(orcamento);
+		}
+	}
+
+	protected abstract boolean deveUsarMaximaTaxacao(Orcamento orcamento);
+
+	protected abstract double maximaTaxacao(Orcamento orcamento);
+
+	protected abstract double minimaTaxacao(Orcamento orcamento);
+}
